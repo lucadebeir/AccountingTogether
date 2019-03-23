888b9a39f18f7bbe78057c80c7903430a37d3dce
lucadebeir <l.debeir@me.com>
2019-03-23 12:24:24 +0100

    first commit

---
+++ AccountingTogether/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AccountingTogether
+//
+//  Created by Luca Debeir on 13/03/2019.
+//  Copyright © 2019 Luca Debeir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
