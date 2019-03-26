b6c7eea6ea6fc1fc6fed2528ce786b9789a6037a
Luca Debeir <l.debeir@me.com>
2019-03-26 10:08:46 +0100

    ajout cell

---
+++ AccountingTogether/Cells/TravellerTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TravellerTableViewCell.swift
+//  AccountingTogether
+//
+//  Created by Luca Debeir on 26/03/2019.
+//  Copyright © 2019 Luca Debeir. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TravellerTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var nameTraveller: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
