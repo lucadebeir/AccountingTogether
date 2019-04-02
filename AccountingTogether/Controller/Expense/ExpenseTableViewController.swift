7d8c5673ad005a491539fada6be671593ca349ff
serafin <serafinsolene@gmail.com>
2019-04-03 00:33:58 +0200

    contraintes vues pour Ipad et Iphone

---
+++ AccountingTogether/Controller/Expense/ExpenseTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ExpenseTableViewController.swift
+//  AccountingTogether
+//
+//  Created by Paire on 02/04/2019.
+//  Copyright © 2019 Luca Debeir. All rights reserved.
+//
+
+import UIKit
+
+class ExpenseTableViewController: UITableViewDataSource {
+
+}
